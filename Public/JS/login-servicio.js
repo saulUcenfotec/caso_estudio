1e397044d1420d89aa5355ce78d8c09995c34ad0
saulUcenfotec <srojase@ucenfotec.ac.cr>
2024-02-29 18:59:59 -0600

    login
    comunicacion de frontend con el backend

--- Public/JS/login-servicio.js
+++ Public/JS/login-servicio.js
@@ -4,72 +4,31 @@ var url = "";
 
 const validarCredencialesCliente = async(pNombre, pContrasena) => {
     url = 'http://localhost:3000/usuarios/login';
-    const datos = {
-        nombre: pNombre,
-        contrasena: pContrasena
-    };
+
+
+    const formData = new FormData();
+    formData.append('nombre', pNombre);
+    formData.append('contrasena', pContrasena);
     const opciones = {
         method: 'POST', // Método de la solicitud
-        headers: {
-            'Content-Type': 'application/json' // Tipo de contenido de la solicitud
-        },
-        body: JSON.stringify(datos) // Convertir el objeto de datos a JSON
+
+        body: formData // Convertir el objeto de datos a JSON
     };
 
-    console.log(datos);
     let usuario;
     await fetch(url, opciones)
         .then(response => {
-            return response.json();
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                return null;
+            }
         }).then(data => {
-            console.log('hooo');
-            console.log(data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-
-    return usuario;
-};
-
-const validarCredencialesModerador = async(pCorreo, pContrasenna) => {
-    let respuesta = '';
-    let usuario;
-    await axios({
-            method: 'post',
-            url: 'http://localhost:3000/api/inicio-sesion_mod',
-            responseType: 'json',
-            data: {
-                correoEncEmp: pCorreo,
-                contrasennaEmp: pContrasenna
-            }
-        })
-        .then(response => {
-            sessionStorage.setItem('conectado', response.success);
-            usuario = response.data.usuario;
-        })
-        .catch(error => {
-            console.log(error);
-        });
-
-    return usuario;
-};
-
-const validarCredencialesAdmin = async(pNombre, pContrasenna) => {
-    let respuesta = '';
-    let usuario;
-    await axios({
-            method: 'post',
-            url: 'http://localhost:3000/api/inicio-sesion_admin',
-            responseType: 'json',
-            data: {
-                correo: pNombre,
-                contrasenna: pContrasenna
-            }
-        })
-        .then(response => {
-            sessionStorage.setItem('conectado', response.success);
-            usuario = response.data.usuario;
+            if (data != null) {
+                localStorage.setItem('usuario', data);
+                window.location.href = '/';
+                usuario = data;
+            } else {}
         })
         .catch(error => {
             console.log(error);
